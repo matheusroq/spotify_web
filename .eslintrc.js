ce43f23afb95a17b565cec57cc9bab5b02306f01
Matheus Roque <matheusfariasroq@gmail.com>
2021-11-15 14:52:44 -0300

    alter eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
-  rules: {},
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
+  },
 };
