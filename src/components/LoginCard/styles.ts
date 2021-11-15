782c3593f6a00e23c77951d7f2899455f2080d01
Matheus Roque <matheusfariasroq@gmail.com>
2021-11-15 19:37:48 -0300

    alter login card style and add image to background

--- src/components/LoginCard/styles.ts
+++ src/components/LoginCard/styles.ts
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
+import img from '../../assets/bg.jpg';
 
 export const Container = styled.div`
-  margin: 20px;
-  width: 50vw;
-  height: 50vh;
-  border: 5px solid #fff;
-  border-radius: 5px;
+  margin: 10px;
+  height: 80vh;
   min-width: 300px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 2px;
+  background: url(${img}) no-repeat center;
+  filter: brightness(1);
+  background-size: 500px;
 `;
